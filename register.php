96dd963b1d1cfb67f2a77d1b5983e47b6ba4b858
Jesus Hilario H <jesushilarioh@users.noreply.github.com>
2018-05-11 16:02:32 -0500

    Update register.php

--- register.php
+++ register.php
@@ -50,8 +50,8 @@
     // Validate password
     if(empty($password)){
       $password_err = 'Please enter password';
-    } else if(strlen($password) < 6){
-      $password_err = 'Password must be at least 6 characters '. 'PASSWORD LENGTH IS: '. strlen($password);
+    } elseif(strlen($password) < 6){
+      $password_err = 'Password must be at least 6 characters ';
     }
 
     // Validate Confirm password
